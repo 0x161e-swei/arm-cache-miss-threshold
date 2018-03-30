d9b428f2f6a3efd1f5dc48c3693e194d645f8b2d
Shijia Wei <shijiawei@utexas.edu>
2018-03-31 04:36:47 +1030

    minor

--- cache_miss_threshold.cc
+++ cache_miss_threshold.cc
@@ -19,6 +19,7 @@
 
 #define ARR_SIZE (1024*1024*8)
 #define PAGE_SIZE (4096/sizeof(uint32_t))
+#define NUM_MEASURES 10
 
 int main() {
     srand(time(NULL));
@@ -28,13 +29,14 @@ int main() {
     uint64_t tic = 0, toc = 0;
     arm_timing_init(false);
     for (int i = 0; i < 10; i++) {
-        asm volatile ("DSB; ISB");
         tic = arm_get_timing();
         arm_memory_barrier();
+        
         arm_access_memory((void *)ptr);
+       
         arm_memory_barrier();
         toc = arm_get_timing();
-        printf("hit: %llu %llu %llu\n", toc - tic, tic, toc);
+        printf("hit: %llu\n", toc - tic);
     }
    
     // this prevents TLB Miss, etc
@@ -43,13 +45,14 @@ int main() {
     arm_access_memory((void *)(&marr[ppn * PAGE_SIZE]));
 
     ptr = marr + (ppn * PAGE_SIZE + rand() % PAGE_SIZE);
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < NUM_MEASURES; i++) {
         tic = arm_get_timing();
         arm_memory_barrier();
+        
         arm_access_memory((void *)ptr);
+       
         arm_memory_barrier();
         toc = arm_get_timing();
-        arm_memory_barrier();
         ptr = marr + (ppn * PAGE_SIZE + rand() % PAGE_SIZE);
         printf("random access %p: %llu\n", ptr, toc - tic);
     }
